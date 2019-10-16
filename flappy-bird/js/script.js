858dc1cb369e9561f63d9e7d6495393820c29ce7
Saroj <sarojpaudyal53@gmail.com>
2019-10-16 22:33:14 +0545

    bug fixes

--- flappy-bird/js/script.js
+++ flappy-bird/js/script.js
@@ -5,6 +5,6 @@ var game = new Game(gameContainer, 2, 4, 37.5, 60);
 game.startGame();
 
 
-var gameContainer1 = document.getElementsByClassName('game-container')[1];
-var game1 = new Game(gameContainer1, 2, 4, 37.5, 60);
-game1.startGame();
+// var gameContainer1 = document.getElementsByClassName('game-container')[1];
+// var game1 = new Game(gameContainer1, 2, 4, 37.5, 60);
+// game1.startGame();
