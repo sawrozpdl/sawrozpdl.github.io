5fd2cb29d103c8208e8c0fbc8891753756310480
Saroj Paudyal <Sarojpaudyal123@gmail.com>
2019-11-19 14:01:30 +0545

    bug fixes

--- ignore/script.js
+++ ignore/script.js
@@ -5,24 +5,24 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://api.github.com/users/sawrozpdl/repos', true);
 request.onload = function () {
   var data = JSON.parse(this.response); //this.response is basically the data received which is arraylist of JSON
-  if (request.status >= 200 && request.status < 400) {
+  if (request.status >= 200 || request.status < 400) {
     data.forEach(repo => {
       
-      var title = repo.name;
-      var directUrl = "https://sawrozpdl.github.io/" + title;
-      var htmlUrl = repo.html_url;
-      var desc = repo.description;
+      let title = repo.name;
+      let directUrl = "https://sawrozpdl.github.io/" + title;
+      let htmlUrl = repo.html_url;
+      let desc = repo.description;
       repoCounter++;
-      var status = "In-Development";
-      var colorClass = "red"; 
-      var keywords = desc.split(" ");
+      let status = "In-Development";
+      let colorClass = "red"; 
+      let keywords = [];
+      if (desc) keywords = desc.split(" ");
       if (keywords.includes("completed")) {
           status = "Completed";
           colorClass = "green";
       }
       
-
-      var row = document.createElement('tr');
+      let row = document.createElement('tr');
       row.innerHTML = "<td>" + title + `</td> <td> <a href="${directUrl}">Link<a/> </td> <td class = "${colorClass}">` + status + `</td> <td> <a href = "${htmlUrl}">Link</a></td>`;
       table.appendChild(row);
     });
